c597f3afbef520e26c9498cfeeaa93d32029e2ff
Ashita-09 <ashitanema2089@gmail.com>
2021-02-14 16:03:24 +0530

    Second

--- myexam/views.py
+++ myexam/views.py
@@ -5,34 +5,10 @@ from django.http import HttpResponse
 # Create your views here.
 
 def HomePage(request):
-    os.system("wssh  --policy=reject")
+    
     return render(request, "Home.html", None)
 
-def Terminal(request):
-    
-    return render(request, "Terminal.html", None)
 
 def Split(request):
-    
+    os.system("wssh")
     return render(request, "Split.html", None)
-
-
-def Step3(request):
-    
-    return render(request, "Step3.html", None)
-
-def Step2(request):
-    
-    return render(request, "Step2.html", None)
-
-def Step4(request):
-    
-    return render(request, "Step4.html", None)
-
-def Step5(request):
-    
-    return render(request, "Step5.html", None)
-
-def Step6(request):
-    
-    return render(request, "Step6.html", None)
